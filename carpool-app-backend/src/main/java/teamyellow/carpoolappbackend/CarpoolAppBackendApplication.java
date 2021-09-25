3596745c239b1804bee7a6f8fac36c2e8d5fde24
monakona <37087754+monakona@users.noreply.github.com>
2021-09-25 01:20:15 -0500

    Initial commit for spring boot app

---
+++ carpool-app-backend/src/main/java/teamyellow/carpoolappbackend/CarpoolAppBackendApplication.java
@@ -0,0 +1,13 @@
+package teamyellow.carpoolappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarpoolAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarpoolAppBackendApplication.class, args);
+	}
+
+}
---
+++ carpool-app-backend/src/test/java/teamyellow/carpoolappbackend/CarpoolAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package teamyellow.carpoolappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarpoolAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
